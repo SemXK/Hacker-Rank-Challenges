3ddbb5085acbdd68f355656c3241cdead8087e1a
SemXK <72094409+SemXK@users.noreply.github.com>
2023-08-11 23:34:53 +0200

    3/30

--- 30Dchallenge/3.js
+++ 30Dchallenge/3.js
@@ -1,6 +1,6 @@
 function main(N) {
   console.log(N>6 && N <20)
-  if(N%2 !==0 ) {          //N is ODD
+  if(N%2 !==0 ) {          //N is ODD 
     console.log(N, "Weird")
   }
   else if(N>5 && N<20) {      //N is in range (5, 20]
